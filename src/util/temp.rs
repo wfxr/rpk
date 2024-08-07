0b6855200072944e5b5f4bf203559ca5a383502e
Wenxuan Zhang <wenxuangm@gmail.com>
2024-08-07 23:33:49 +0800

    fix
    Signed-off-by: Wenxuan Zhang <wenxuangm@gmail.com>

--- src/util/temp.rs
+++ src/util/temp.rs
@@ -1,8 +1,10 @@
-use std::ffi;
-use std::fs;
-use std::io;
-use std::path::{Path, PathBuf};
-use std::result;
+use std::{
+    ffi,
+    fs,
+    io,
+    path::{Path, PathBuf},
+    result,
+};
 
 use anyhow::Result;
 
